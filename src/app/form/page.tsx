c9b4ce621fb18a97552ff1c6f913abf9ae91039e
Ulsbold12 <ulsboldulsbold921@gmail.com>
2025-12-10 09:01:34 +0800

    firstcommit

---
+++ src/app/form/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { Card, CardAction, CardContent, CardHeader } from "@/components/ui/card";
+import {z} from 
+
+export const Form = () => {
+  return (
+    <>
+      <div className="w-screen h-screen flex justify-center items-center">
+        <Card className="w-100">
+          <CardHeader>
+            <h1 className="text-2xl font-bold">User form</h1>
+          </CardHeader>
+
+
+          <CardContent>
+            <Form>
+
+            </Form>
+          </CardContent>
+        </Card>
+      </div>
+    </>
+  );
+};
